c1be213eb7c99f64b6c28c95cd09639b75d176b9
NK044575 <ning.kang@cerner.com>
2016-09-29 18:01:53 -0500

    Added AvlTree class without implementation

---
+++ src/ds/tree/bst/avltree/AvlTree.java
@@ -0,0 +1,36 @@
+package ds.tree.bst.avltree;
+
+import ds.tree.TreeNode;
+import ds.tree.bst.BinarySearchTree;
+
+public class AvlTree<K extends Comparable<K>, T> implements BinarySearchTree<K, T>{
+  @Override
+  public TreeNode<K, T> search(K k) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public void insert(TreeNode<K, T> n) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void delete(TreeNode<K, T> n) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void delete(K k) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void printTree() {
+    // TODO Auto-generated method stub
+    
+  }
+}
