8d319f580a199f5d3ad8b026422d6f55044cbdcc
NK044575 <ning.kang@cerner.com>
2016-10-05 14:30:04 -0500

    Added question about binary search tree traversal, need to fill in implementation

---
+++ src/my/question/tree/FindKthElement.java
@@ -0,0 +1,24 @@
+package my.question.tree;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FindKthElement {
+  public Node findCommonAncestor(Node root, Node p, Node q){
+    
+    return null;
+  }
+  
+  public Node findKthSmallestNode(Node root, int k){
+    Map<Node, Boolean> visited = new HashMap<>();
+    
+    return null;
+  }
+  
+  private static class Node{
+    int val;
+    Node left;
+    Node right;
+    Node(int x){val = x;}
+  }
+}
