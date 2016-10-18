edb2fbf72cd080251392a2a95d05f91d0f788fe0
NK044575 <ning.kang@cerner.com>
2016-10-18 17:17:50 -0500

    Shortened comparator lambda expression by eliminating the parameter type

--- src/simpletests/algorithm/sort/generic/CollectionsSortMain.java
+++ src/simpletests/algorithm/sort/generic/CollectionsSortMain.java
@@ -7,6 +7,6 @@ import utility.TraversableUtil;
 
 public class CollectionsSortMain {
   public static void main(String[] argv){
-    console(CollectionsSort.sort(TraversableUtil.unsortedIntegerList(), (Integer i1, Integer i2) -> i1.compareTo(i2)));
+    console(CollectionsSort.sort(TraversableUtil.unsortedIntegerList(), (i1, i2) -> i1.compareTo(i2)));
   }
 }
