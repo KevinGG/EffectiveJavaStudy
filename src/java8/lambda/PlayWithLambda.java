b393bd81e36648d14adf51958368384aa88295a7
KevinGG <kawaigin@gmail.com>
2016-10-23 18:13:01 -0500

    Added ToIntFunction realization for Comparator

--- src/java8/lambda/PlayWithLambda.java
+++ src/java8/lambda/PlayWithLambda.java
@@ -3,18 +3,22 @@ package java8.lambda;
 import static java.lang.System.out;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.function.Function;
+import java.util.function.ToIntFunction;
 
 public class PlayWithLambda {
   public static void main(String[] argv){
     Function<String, Integer> length = (s) -> s.length();
     out.println(length.apply("abc"));
     
-    Function<Integer, Integer> negative = i -> -i;
-    List<Integer> list = Arrays.asList(6,5,4,3,2,1);
+    ToIntFunction<Integer> negative = i -> -i;
+    List<Integer> list = Arrays.asList(1,2,3,4,5,6);
     list.sort(Comparator.comparingInt(negative));
+    //list.sort(Collections.reverseOrder());
+    Function<Integer, Integer> reverse = i -> -i;
     System.out.println(list);
   }
 }
