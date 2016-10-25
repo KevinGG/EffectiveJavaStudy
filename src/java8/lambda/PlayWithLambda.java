270411338859609bfd962c7bfda2aa1d8d3b46c5
NK044575 <ning.kang@cerner.com>
2016-10-25 17:48:01 -0500

    Play with lambda and stream

--- src/java8/lambda/PlayWithLambda.java
+++ src/java8/lambda/PlayWithLambda.java
@@ -3,22 +3,71 @@ package java8.lambda;
 import static java.lang.System.out;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.function.Function;
+import java.util.function.IntFunction;
+import java.util.function.IntUnaryOperator;
 import java.util.function.ToIntFunction;
+import java.util.stream.Collectors;
 
 public class PlayWithLambda {
-  public static void main(String[] argv){
+  public static int reverseOrder(Integer i) {
+    return -i;
+  }
+
+  public static int naturalOrder(Integer i) {
+    return i;
+  }
+
+  public static int reverseOrderFromInt(int i) {
+    return -i;
+  }
+
+  public static int length(String s) {
+    return s.length();
+  }
+
+  public static void main(String[] argv) {
+    // a Function interface of input type String and output type Integer,
+    // implemented with lambda expression, return a String's length
     Function<String, Integer> length = (s) -> s.length();
+    // apply length function to "abc" String, return "abc"'s length
     out.println(length.apply("abc"));
+
+    List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 100);
+
+    // a Function interface of input type Integer and output type primitive int
+    Function<Integer, Integer> negativeFromIntegerToInteger = i -> -i;
+    ToIntFunction<Integer> negativeFromIntegerToInt = i -> -i;
+    IntFunction<Integer> negativeFromIntToInteger = i -> -i;
+    IntUnaryOperator negativeFromIntToInt = i -> -i;
+
+    list.sort(Comparator.comparing(negativeFromIntegerToInteger));
+    list.sort(Comparator.comparingInt(negativeFromIntegerToInt));
+    // all from int Function cannot be used here, compile error
+    // list.sort(Comparator.comparing(negativeFromIntToInteger));
+    // list.sort(Comparator.comparingInt(negativeFromIntToInt));
+
+    list.sort(Comparator.comparing(PlayWithLambda::reverseOrder));
+    out.println(list);
+
+    list.sort(Comparator.comparing(PlayWithLambda::naturalOrder));
+    out.println(list);
+
+    // if using method reference, the input type and output type could both be primitive
+    list.sort(Comparator.comparing(PlayWithLambda::reverseOrderFromInt));
+    out.println(list);
+
+    // the comparator is for String not int, so compile error
+    // list.sort(Comparator.comparing(PlayWithLambda::length));
     
-    ToIntFunction<Integer> negative = i -> -i;
-    List<Integer> list = Arrays.asList(1,2,3,4,5,6);
-    list.sort(Comparator.comparingInt(negative));
-    //list.sort(Collections.reverseOrder());
-    Function<Integer, Integer> reverse = i -> -i;
-    System.out.println(list);
+    // sort by int String representation length
+    System.out.println(list.stream().map(i -> String.valueOf(i))
+        .sorted(Comparator.comparingInt(PlayWithLambda::length)).collect(Collectors.toList()));
+    
+    // reversed sort by int String representation length
+    System.out.println(list.stream().map(i -> String.valueOf(i))
+        .sorted(Comparator.comparingInt(PlayWithLambda::length).reversed()).collect(Collectors.toList()));
   }
 }
--- src/java8/stream/PlayWithStream.java
+++ src/java8/stream/PlayWithStream.java
@@ -1,11 +1,22 @@
 package java8.stream;
 
+import java.util.ArrayList;
 import java.util.ConcurrentModificationException;
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import utility.TraversableUtil;
 
 public class PlayWithStream {
+  public static final String toString(Integer i) {
+    return String.valueOf(i);
+  }
+
+  public static final Integer minus1(Integer i) {
+    return i - 1;
+  }
+
   public static void main(String[] argv) {
     List<Integer> list = TraversableUtil.unsortedIntegerList();
     System.out.println(list);
@@ -21,10 +32,46 @@ public class PlayWithStream {
     list.stream().forEach(i -> list.set(list.size() - 1, i));
     System.out.println(list);
 
+    Function<String, Boolean> function = s -> s.equals("5");
+
+    // try map, filter and collect
+    List<Integer> collectorList =
+        list.stream().map(PlayWithStream::minus1).collect(Collectors.toList());
+    System.out.println(collectorList);
+    System.out.println("======================================================================");
+    System.out.println(list.stream().map(PlayWithStream::toString).filter(s -> s.length() == 1)
+        .map(function).collect(Collectors.toList()));
+
+    // never do anything after following codes since the exceptions thrown by the statements are
+    // suppressed by catch block, the current list's contents are unexpected
+    // structural modifications, throw ConcurrentModificationException
+    // However, the following codes will provide us with an understanding of how different iterating
+    // mechanics iterate a given collection or stream
     try {
       list.forEach(i -> list.add(i));
     } catch (ConcurrentModificationException cme) {
       System.out.println("ConcurrentModificationException");
     }
+    // if suppressing ConcurrentModificationException of Iterable<T>::forEach, the last element will
+    // be successfully added
+    System.out.println(list);
+
+    try {
+      list.stream().forEach(i -> list.add(i));
+    } catch (ConcurrentModificationException cme) {
+      System.out.println("ConcurrentModificationException");
+    }
+    // if suppressing ConcurrentModificationException of Stream<T>::forEach, the resulted list will
+    // be [xx] if original list is [x].
+    System.out.println(list);
+
+    try {
+      list.parallelStream().forEach(i -> list.add(i));
+    } catch (ConcurrentModificationException cme) {
+      System.out.println("ConcurrentModificationException");
+    }
+    // if suppressing ConcurrentModificationException of Stream<T>::forEach, the resulted list will
+    // be [xx] if original list is [x].
+    System.out.println(list);
   }
 }
