412c0e39cc305535ef1f981df56c316515c1be8f
NK044575 <ning.kang@cerner.com>
2016-10-31 17:06:48 -0500

    Added question to search repeat sub trees in a binary tree

---
+++ src/my/question/tree/SearchRepeatSubTree.java
@@ -0,0 +1,19 @@
+package my.question.tree;
+
+import java.util.HashSet;
+import java.util.Set;
+
+
+public class SearchRepeatSubTree {
+  public Set<Set<TreeNode>> findRepeatSubTrees(TreeNode root){
+    Set<Set<TreeNode>> result = new HashSet<>();
+    
+    return result;
+  }
+  
+  private static final class TreeNode{
+    int val;
+    TreeNode left;
+    TreeNode right;
+  }
+}
